5d4615b7cdf6dbb68c0326af9212259e5453b72f
research1-alt <research1@omegaseikimobility.com>
2026-02-17 11:25:24 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-
-const CACHE_NAME = 'osm-live-v1';
-const urlsToCache = [
+const CACHE_NAME = 'osm-live-cache-v1';
+const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/index.tsx',
+  'https://cdn.tailwindcss.com'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
